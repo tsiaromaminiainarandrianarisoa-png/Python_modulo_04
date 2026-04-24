37a61833bfd3b1f2ffbbfccc321add387250ad4e
tsiaro <tsiaromaminiainarandrianarisoa@gmail.com>
2026-04-24 13:54:03 +0000

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,23 @@
+import sys
+import typing
+
+def display_contents()-> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    print("=== Cyber Archives Recovery ===")
+    file_name = sys.argv[1]
+    try:
+        print(f"Accessing file '{file_name}'")
+        fd = open(file_name)
+    except OSError as error:
+        print(f"Error opening file '{file_name}': {error}")
+    else:
+        print("---\n")
+        print(fd.read())
+        print(f"\n---\nFile '{file_name}' closed.")
+        fd.close()
+
+if __name__ == "__main__":
+    display_contents()
+
