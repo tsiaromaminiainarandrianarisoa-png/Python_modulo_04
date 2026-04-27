8b07bfc9c4e038577fac14c474f7ee7da67bd816
Tsiaro <tsiaromaminiainarandrianarisoa@gmail.com>
2026-04-27 12:55:38 +0300

    ex1

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,40 @@
+import sys
+import typing
+
+def display_contents()-> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    print("=== Cyber Archives Recovery ===")
+    file_name = sys.argv[1]
+    try:
+        print(f"Accessing file '{file_name}'")
+        f = open(file_name)
+    except OSError as error:
+        print(f"Error opening file '{file_name}': {error}")
+    else:
+        print("---")
+        content = f.read()
+        print(content)
+        print(f"\n---\nFile '{file_name}' closed.")
+        print("\nTransform data:\n---\n")
+        f.close()
+        contents = content.rsplit('\n')
+        new_contents = contents[0]
+        for line in contents[1:]:
+            new_contents = "#\n".join([new_contents, line])
+        print(f"{new_contents}\n---")
+        new_file = input("Enter new file name (or empty): ")
+        if not new_file:
+            print("Not saving data.")
+        else:
+            nf = open(new_file, "w")
+            nf.write(new_contents)
+            nf.close()
+            print(f"Saving data to '{new_file}'")
+            print(f"Data saved in file '{new_file}'.\n")
+
+if __name__ == "__main__":
+     display_contents()
+
+
